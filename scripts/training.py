3a636d313894339e85a97a4c27f2d0abb21a412e
Lucas Moraes <77943277+Hotarouuu@users.noreply.github.com>
2025-07-11 13:17:08 -0300

    Update 2.1
    - test model added

--- scripts/training.py
+++ scripts/training.py
@@ -3,63 +3,77 @@ from dotenv import load_dotenv
 import os
 import torch
 import joblib
-
-
 load_dotenv()  
 
-dataset_path = os.getenv("data_dir") # Define variables with .env or add path here
-models_path = os.getenv('models')
+# Preparing the paths
 
+## Only models
+models_path = os.getenv('models')
+encoding_path = os.path.join(models_path, "encoding_models")
+
+## Only the datasets
+
+dataset_path = os.getenv("data_dir") 
 dataset = os.path.join(dataset_path, "processed")
 dataset_ratings = os.path.join(dataset, "Books_rating.csv")
 dataset_books = os.path.join(dataset, "books_data.csv")
 
-encoding_path = os.path.join(models_path, "encoding_models")
 
-print('Initializing training script...\n')
+def main():
+    
+        # Treating data
 
-df = data_treatment(dataset_books, dataset_ratings)
+        df = data_treatment(dataset_books, dataset_ratings)
 
-proce = Processor(df)
+        # Processing 
 
-# We need the encoders, because we can't initialize one everytime
+        proce = Processor(df)
 
-_ ,ordinal_encoder, authors_encoder, gender_encoder = proce.encoding()
+        _ ,ordinal_encoder, authors_encoder, gender_encoder = proce.encoding()
 
-print(f'Saving the encoders')
+        print(f'Saving the encoders')
 
-joblib.dump(ordinal_encoder, os.path.join(encoding_path, 'ordinal_encoder.joblib'))
-joblib.dump(authors_encoder, os.path.join(encoding_path, 'authors_encoder.joblib'))
-joblib.dump(gender_encoder, os.path.join(encoding_path, 'gender_encoder.joblib'))
+        joblib.dump(ordinal_encoder, os.path.join(encoding_path, 'ordinal_encoder.joblib'))
+        joblib.dump(authors_encoder, os.path.join(encoding_path, 'authors_encoder.joblib'))
+        joblib.dump(gender_encoder, os.path.join(encoding_path, 'gender_encoder.joblib'))
 
-print('Done!\n')
+        print('Done!\n')
 
-trainloader, testloader, evalloader, n_users, n_items, n_genders, n_authors = proce.run_pipeline()
+        # Preparing to the training 
+
+        trainloader, testloader, evalloader, n_users, n_items, n_genders, n_authors = proce.run_pipeline()
 
 
-# Training
-print('The training is starting!\n ')
+        # Training
+        print('The training is starting!\n ')
 
-model = NeuMF(n_users, n_items,n_genders, n_authors, device='cuda', n_factors=16)
+        model = NeuMF(n_users, n_items,n_genders, n_authors, device='cuda', n_factors=16)
 
-train = trainer(model, 
-        trainloader, 
-        evalloader, 
-        device = 'cuda', 
-        early_stopping = True, 
-        n_factors = 16, 
-        lr = 0.0005, 
-        weight_decay = 1e-5)
+        train = trainer(model, 
+                trainloader, 
+                evalloader, 
+                device = 'cuda', 
+                early_stopping = True, 
+                n_factors = 16, 
+                lr = 0.0005, 
+                weight_decay = 1e-5)
 
-# Evaluation
+        # Evaluation
 
-k = 10
+        k = 10
 
-avg_precision, avg_recall, f_score, user_item_scores= evaluate_batch_precision_recall(testloader, model, k=k)
+        avg_precision, avg_recall, f_score, user_item_scores= evaluate_batch_precision_recall(testloader, model, k=k)
 
-print(f"Precision@{k}: {avg_precision * 100:.4f}%")
-print(f"Recall@{k}: {avg_recall * 100:.4f}%")
-print(f'F-Score@{k}: {f_score * 100:4f}%')
+        print(f"Precision@{k}: {avg_precision * 100:.4f}%")
+        print(f"Recall@{k}: {avg_recall * 100:.4f}%")
+        print(f'F-Score@{k}: {f_score * 100:4f}%')
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
 
